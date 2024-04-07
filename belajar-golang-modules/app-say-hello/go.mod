d098f559c8f04150d45caab237531a498107f0a5
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2024-04-07 21:24:13 +0700

    initial commit

---
+++ belajar-golang-modules/app-say-hello/go.mod
@@ -0,0 +1,5 @@
+module github.com/harmonify/app-say-hello
+
+go 1.22.2
+
+require github.com/harmonify/go-say-hello v1.1.0 // indirect
---
+++ belajar-golang-modules/app-say-hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/harmonify/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
